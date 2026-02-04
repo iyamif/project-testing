308a20ad0e262d46da1938b68e47cf7ae86e0fc3
Ilham Firmansyah <iyamif@gmail.com>
2026-02-04 17:04:30 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
         <div class="space-y-1">
             <div class="flex items-center gap-2 text-indigo-400 font-bold tracking-widest text-xs uppercase">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/></svg>
-                Korlantas Polri
+                Korlantas PolriIIII
             </div>
             <h1 class="text-3xl lg:text-4xl font-extrabold tracking-tight text-white">
                 Operasi Ketupat <span class="text-indigo-500">2024</span>
